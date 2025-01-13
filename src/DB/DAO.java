43c48eef6a26a6865ae42f14ebe7c8b2f6630109
Hazem-web <hazem.mah2722@gmail.com>
2025-01-13 21:26:43 +0200

    add server status database functions

--- src/DB/DAO.java
+++ src/DB/DAO.java
@@ -3,6 +3,7 @@ package DB;
 import DTO.DTOPlayer;
 import org.apache.derby.jdbc.ClientDriver;
 import java.sql.*;
+import tictactoegameitiserver.MassageType;
 
 public class DAO {
 
@@ -22,5 +23,49 @@ public class DAO {
         con.close();
         ps.close();
         return exists;
+    }
+    public static int getAvailablePlayersForServer() throws SQLException{
+        int number=0;
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Player",
+                "root", "root");
+        PreparedStatement ps = con.prepareStatement("select COUNT(*) from PLAYER where STAUS = ?",
+                ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        ps.setString(1, MassageType.STATUS_ONLINE);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            number=rs.getInt(1);
+        }
+        return number;
+    }
+    
+    public static int getOfflinePlayersForServer() throws SQLException{
+        int number=0;
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Player",
+                "root", "root");
+        PreparedStatement ps = con.prepareStatement("select COUNT(*) from PLAYER where STAUS = ?",
+                ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        ps.setString(1, MassageType.STATUS_OFFLINE);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            number=rs.getInt(1);
+        }
+        return number;
+    }
+    
+    public static int getInGamePlayersForServer() throws SQLException{
+        int number=0;
+        DriverManager.registerDriver(new ClientDriver());
+        Connection con = DriverManager.getConnection("jdbc:derby://localhost:1527/Player",
+                "root", "root");
+        PreparedStatement ps = con.prepareStatement("select COUNT(*) from PLAYER where STAUS = ?",
+                ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+        ps.setString(1, MassageType.STATUS_INGAME);
+        ResultSet rs = ps.executeQuery();
+        if(rs.next()){
+            number=rs.getInt(1);
+        }
+        return number;
     }
 }
