f0834f1432679935e709648d20d1a7a24be0ccf8
Aya <ayaemam471@gmail.com>
2025-01-15 19:46:54 +0200

    sign up

--- src/tictactoegameitiserver/MassageType.java
+++ src/tictactoegameitiserver/MassageType.java
@@ -10,20 +10,23 @@ package tictactoegameitiserver;
  * @author HAZEM-LAB
  */
 public class MassageType {
-    public static final String LOGIN_MSG="login";
-    public static final String LOGINSUCCESS_MSG="login successful";
-    public static final String LOGINFAIL_MSG="login failed";
-    public static final String REGISTER_MSG="register";
-    public static final String UPDATE_LIST_MSG="update";
-    public static final String SERVER_CLOSE_MSG="server close";
-    public static final String CHALLENGE_REQUEST_MSG="challenge req";
-    public static final String CHALLENGE_ACCESSEPT_MSG="challenge acc";
-    public static final String PLAY_MSG="play";
-    public static final String END_GAME_MSG="end game";
-    public static final String RESTART_GAME_MSG="restart game";
-    public static final String CLIENT_CLOSE_MSG="client close";
-    
-    public static final String STATUS_ONLINE="online";
-    public static final String STATUS_OFFLINE="offline";
-    public static final String STATUS_INGAME="in game";
+
+    public static final String LOGIN_MSG = "login";
+    public static final String LOGINSUCCESS_MSG = "login successful";
+    public static final String LOGINFAIL_MSG = "login failed";
+    public static final String REGISTER_MSG = "register";
+    public static final String REGISTER_SUCCESS_MSG = "register succeccful";
+    public static final String REGISTER_FAIL_MSG = "register failed";
+    public static final String UPDATE_LIST_MSG = "update";
+    public static final String SERVER_CLOSE_MSG = "server close";
+    public static final String CHALLENGE_REQUEST_MSG = "challenge req";
+    public static final String CHALLENGE_ACCESSEPT_MSG = "challenge acc";
+    public static final String PLAY_MSG = "play";
+    public static final String END_GAME_MSG = "end game";
+    public static final String RESTART_GAME_MSG = "restart game";
+    public static final String CLIENT_CLOSE_MSG = "client close";
+
+    public static final String STATUS_ONLINE = "online";
+    public static final String STATUS_OFFLINE = "offline";
+    public static final String STATUS_INGAME = "in game";
 }
